30d7976013cfbd070c725cb6217e3b5ebe57876c
80001105 <80001105@94bdb878-8ede-a643-b4a6-981fb481ccc0>
2016-04-22 11:10:21 +0000

    commit compataibility of check attendance
    git-svn-id: http://10.116.50.190/svn/dev-spms-core/branches/code/V4.0@4685 94bdb878-8ede-a643-b4a6-981fb481ccc0

---
+++ V4.0/rollback/1_rollback_drop_table.sql
@@ -0,0 +1,9 @@
+drop table  TT_SCHEDULE_DAILY_HIS ;
+
+drop table TT_SAP_SYNCHRONOUS_HIS ;
+
+drop table  TT_PB_SHEDULE_BY_MONTH_HIS ;
+
+drop table TT_PB_SHEDULE_BY_DAY_HIS ;
+
+drop table TT_DRIVER_SCHEDULING_HIS ;
