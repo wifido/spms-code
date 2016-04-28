b9fbef52a66842edae08a7977d3dc85bc2d2f577
80000509 <80000509@94bdb878-8ede-a643-b4a6-981fb481ccc0>
2016-04-28 09:49:22 +0000

    [sfit0509] -N/A add tree query dept code
    git-svn-id: http://10.116.50.190/svn/dev-spms-core/branches/code/V4.0@4750 94bdb878-8ede-a643-b4a6-981fb481ccc0

--- code/web_project/group_project/WebContent/scripts/operation/trainingInfo.js
+++ code/web_project/group_project/WebContent/scripts/operation/trainingInfo.js
@@ -1,6 +1,46 @@
 // <%@ page language="java" contentType="text/html; charset=utf-8"%>
 
 var filterDeptCodeType = '${filterDeptCodeType}';
+
+var fieldDeptCode = new Ext.form.TextField({width:160,name:"fieldDeptCode", xtype : 'textfield'
+	, listeners: {
+        specialkey: function(field, e){
+            if (e.getKey() == e.ENTER) {
+            	queryDeptCode();
+            }
+        }
+    }});
+
+var queryDeptCode = function() {
+	if (fieldDeptCode.getValue()=="") {
+		Ext.Msg.alert('提示','网点代码不能为空', function(){
+			fieldDeptCode.focus();
+		});
+		return;
+	}
+	
+	Ext.Ajax.request({
+		url:"../operation/userDeptAction_queryDeptCode.action",
+		params:{fieldDeptCode:fieldDeptCode.getValue()},
+		success:function(response){
+			//debugger;
+			var dept_ = Ext.util.JSON.decode(response.responseText);
+			var path = dept_.path;
+			if(path && path != '/0'){
+				treePanel.root.reload();
+				treePanel.selectPath(path);
+				Ext.getCmp('query.branchCode').setValue(dept_.deptCode);
+				
+			} else {
+				Ext.Msg.alert('提示','该网点不存在！', function(){
+					fieldDeptCode.selectText();
+				}, this);
+			}
+		} ,scope : this
+	}, false);
+};
+
+
 var treePanel = new Ext.tree.TreePanel({
     region: 'west',
     margins: '1 1 1 1',
@@ -8,6 +48,16 @@ var treePanel = new Ext.tree.TreePanel({
     title: '网点信息',
     collapsible: true,
     autoScroll: true,
+    tbar : [
+	        {text : "网点代码", xtype : 'label'}, 
+	        fieldDeptCode ,
+	        {
+	        	icon : "../images/search.gif", 
+	        	xtype : 'button', 
+	        	style : 'margin-left:2px',
+	        	scope : this,
+	        	handler : queryDeptCode
+	        }], 
     root: new Ext.tree.AsyncTreeNode({
         id: '0',
         text: '顺丰速运',
